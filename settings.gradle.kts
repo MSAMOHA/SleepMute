26439f39a7801879ca3b61f2b50636ec2267ed31
MSAMOHA <9575msa@gmail.com>
2026-09-09 04:13:10 +0200

    Clean up settings.gradle.kts by removing comments
    Removed commented-out plugin and dependency management sections.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,3 @@
-#pluginManagement {
-#    repositories {
-#        google()
-#        mavenCentral()
-#        gradlePluginPortal()
-#   }
-#}
-#dependencyResolutionManagement {
-#   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-#    repositories {
-#        google()
-#        mavenCentral()
-#    }
-#}
-#rootProject.name = "SleepMute"
-#include(":mobile", ":wear")
-
 pluginManagement {
     repositories {
         google()
